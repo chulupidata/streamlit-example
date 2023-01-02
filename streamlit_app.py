51413499267bd70fd3716c178fa73f9607eacdfa
chulupidata <chulupidata@gmail.com>
2023-01-02 09:31:33 +0000

    d
    d

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,38 +1,31 @@
-
-def find_average(values):
-    result = 0
-    for v in values:
-        result += v
-    return result / len(values)
-
-
-print("AVERAGE", find_average([5,6, 7, 8]))
-
-
-
-
-
 import streamlit as st
-from pydub import AudioSegment
-from pydub.silence import split_on_silence
+import librosa
+import librosa.display
+import numpy as np
+import matplotlib.pyplot as plt
+from scipy import signal
+from scipy.io import wavfile
 
 st.title('Noise Reduction Web App')
 
-
+# Add a file uploader widget
 audio_file = st.file_uploader('Upload an audio file:', type='mp3')
 
 if audio_file is not None:
+    # Load the audio file as a waveform
+    rate, data = wavfile.read(audio_file)
 
-audio = AudioSegment.from_mp3(audio_file)
+    # Use a median filter to reduce noise
+    data = signal.medfilt(data, kernel_size=3)
 
+    # Convert the waveform back to an audio file
+    librosa.output.write_wav('noise_reduced.wav', data, rate)
+    audio = AudioSegment.from_wav('noise_reduced.wav')
+    audio.export('noise_reduced.mp3', format='mp3')
 
-segments = split_on_silence(audio, min_silence_len=1000, silence_thresh=-16)
-
-
-concatenated_audio = AudioSegment.silent(duration=0)
-for segment in segments:
-concatenated_audio += segment
-
-
-if st.button('Export as mp3'):
-with open('noise_reduced.
+    # Add a button to download the resulting audio file
+    if st.button('Export as mp3'):
+        with open('noise_reduced.mp3', 'rb') as f:
+            audio_bytes = f.read()
+        st.markdown('Audio exported as **noise_reduced.mp3**')
+        st.audio(audio_bytes, format='audio/mpeg')
