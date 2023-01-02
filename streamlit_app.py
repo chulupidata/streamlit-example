a1e286bd61630052e00bc889c6407f89ad2272dc
chulupidata <chulupidata@gmail.com>
2023-01-02 09:24:03 +0000

    s

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,38 +1,26 @@
-from collections import namedtuple
-import altair as alt
-import math
-import pandas as pd
 import streamlit as st
+from pydub import AudioSegment
+from pydub.silence import split_on_silence
 
-"""
-# Welcome to Streamlit!
+st.title('Noise Reduction Web App')
 
-Edit `/streamlit_app.py` to customize this app to your heart's desire :heart:
+# Add a file uploader widget
+audio_file = st.file_uploader('Upload an audio file:', type='mp3')
 
-If you have any questions, checkout our [documentation](https://docs.streamlit.io) and [community
-forums](https://discuss.streamlit.io).
+if audio_file is not None:
+    # Load the audio file as an AudioSegment object
+    audio = AudioSegment.from_mp3(audio_file)
 
-In the meantime, below is an example of what you can do with just a few lines of code:
-"""
+    # Split the audio file into segments based on silence
+    segments = split_on_silence(audio, min_silence_len=1000, silence_thresh=-16)
 
+    # Concatenate the segments back together
+    concatenated_audio = AudioSegment.silent(duration=0)
+    for segment in segments:
+        concatenated_audio += segment
 
-with st.echo(code_location='below'):
-    total_points = st.slider("Number of points in spiral", 1, 5000, 2000)
-    num_turns = st.slider("Number of turns in spiral", 1, 100, 9)
-
-    Point = namedtuple('Point', 'x y')
-    data = []
-
-    points_per_turn = total_points / num_turns
-
-    for curr_point_num in range(total_points):
-        curr_turn, i = divmod(curr_point_num, points_per_turn)
-        angle = (curr_turn + 1) * 2 * math.pi * i / points_per_turn
-        radius = curr_point_num / total_points
-        x = radius * math.cos(angle)
-        y = radius * math.sin(angle)
-        data.append(Point(x, y))
-
-    st.altair_chart(alt.Chart(pd.DataFrame(data), height=500, width=500)
-        .mark_circle(color='#0068c9', opacity=0.5)
-        .encode(x='x:Q', y='y:Q'))
+    # Add a button to download the resulting audio file
+    if st.button('Export as mp3'):
+        with open('noise_reduced.mp3', 'wb') as f:
+            concatenated_audio.export(f, format='mp3')
+        st.markdown('Audio exported as **noise_reduced.mp3**')
